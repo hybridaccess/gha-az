0fcb340b1d950fbe42aeaed90e2208fa25cbda0e
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-06-15 18:38:07 +0100

    find az location4

--- scripts/cassandra_config.sh
+++ scripts/cassandra_config.sh
@@ -5,6 +5,9 @@ set -ex
 function check_deps() {
   test -f $(which jq) || error_exit "jq command not detected in path, please install it"
   test -f $(which az) || error_exit "az command not detected in path, please install it"
+  pwd
+  ls -al
+  echo $PATH
 }
 function parse_input() {
   eval "$(jq -r '@sh "export AZ_TENANT=\(.tenant_id)"')"
@@ -14,13 +17,11 @@ function parse_input() {
 check_deps
 parse_input
 
-pwd
-ls -al /usr/bin/
 
 # az login --service-principal --username $azuread_application_id --password $azuread_client_secret --tenant $AZ_TENANT
 
 eval "$(jq -r '@sh "RESOURCE_GROUP=\(.resource_group) SUBSCRIPTION_NAME=\(.subscription)"')"
 
-PROPERTIES=$(/usr/bin/az group show --resource-group "$RESOURCE_GROUP" --subscription "$SUBSCRIPTION_NAME" --query "properties" -o json)
+PROPERTIES=$(az group show --resource-group "$RESOURCE_GROUP" --subscription "$SUBSCRIPTION_NAME" --query "properties" -o json)
 
 jq -n --arg properties "$PROPERTIES" '{"properties":$properties}'
